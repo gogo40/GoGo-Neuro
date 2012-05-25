475025e69f8bd743742e31f7a2f6aab3a2991320
unknown <lprad@Lprad-Rafael.(none)>
2012-05-25 02:46:10 -0300

    RBF

---
+++ gen_data.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int N_IN = 20, N = 2000;
+    scanf("%d", &N);
+    int M = N / (1 + N_IN);
+    vector<double> x(N);
+
+    for (int i = 0; i < N; ++i) scanf("%lf", &x[i]);
+
+    printf ("%d\n", M);
+
+    int k = 0;
+
+    for (int i = 0; i < M; ++i){
+        for (int j = 0; j < N_IN; ++j) {
+            printf("%e ", x[k]);
+            ++k;
+        }
+        printf("%e\n", x[k]);
+        ++k;
+    }
+    return 0;
+}
--- rbf.cpp
+++ rbf.cpp
@@ -10,7 +10,7 @@ using namespace std;
 int main(int argc, char** argv) {
     if (argc != 8) {
         fprintf(stderr,"Usage: %s <treino?> <arquivo de treino ou arquivo de teste?> <numero de entradas> <numero de neuronios>"
-                "<numero de saidas> <nome da rede> <raio>\n");
+                "<numero de saidas> <nome da rede> <raio>\n", argv[0]);
         return 0;
     }
 
@@ -47,29 +47,31 @@ int main(int argc, char** argv) {
 
     map<int, bool> marked;
 
-    if (is_train) {
-        FILE* fin = fopen(filename, "r");
-        int N;
+    FILE* fin = fopen(filename, "r");
+    int N;
 
-        fscanf(fin, "%d", &N);
+    fscanf(fin, "%d", &N);
 
-        vector<vector<double> > input(N), output(N);
+    vector<vector<double> > input(N), output(N);
 
-        for (int i = 0; i < N; ++i) {
-            for (int k = 0; k < n_in; ++k) {
-                double x;
-                fscanf(fin, "%lf", &x);
-                input[i].push_back(x);
-            }
-
-            for (int k = 0; k < n_out; ++k) {
-                double y;
-                fscanf(fin, "%lf", &y);
-                output[i].push_back(y);
-            }
+    for (int i = 0; i < N; ++i) {
+        for (int k = 0; k < n_in; ++k) {
+            double x;
+            fscanf(fin, "%lf", &x);
+            input[i].push_back(x);
         }
 
-        fclose(fin);
+        for (int k = 0; k < n_out; ++k) {
+            double y;
+            fscanf(fin, "%lf", &y);
+            output[i].push_back(y);
+        }
+    }
+
+    fclose(fin);
+
+    if (is_train) {
+
 
         for (int q = 0; q < n_neu; ++q) {
             int q1;
@@ -101,10 +103,68 @@ int main(int argc, char** argv) {
                 fprintf(fout, "%e ", m[q][k]);
             }
 
-            fprintf("\n");
+            fprintf(fout, "\n");
         }
         fclose(fout);
     } else {
+        FILE* fout = fopen(outname, "r");
+
+        for (int k = 0; k < n_in; ++k) W[k].resize(n_neu);
+        for (int k = 0; k < n_neu; ++k) m[k].resize(n_out);
+
+        for (int q = 0; q < n_neu; ++q){
+            for (int k = 0; k < n_in; ++k) {
+                fscanf(fout, "%lf", &W[k][q]);
+            }
+
+            for (int k = 0; k < n_out; ++k) {
+                double y;
+                fscanf(fout, "%lf", &y);
+                m[q][k] = y;
+            }
+        }
+        fclose(fout);
+
+        vector<double> u(n_neu), o(n_out);
+
+        FILE* fdes = fopen("sol.dat", "w+");
+        FILE* fnet = fopen("out.dat", "w+");
+
+        double err = 0, errT = 0;
+        for (int i = 0; i < N; ++i) {
+
+            for (int k = 0; k < n_neu; ++k) u[k] = 0;
+
+            for (int k = 0; k < n_out; ++k) o[k] = 0;
+
+            for (int k = 0; k < n_in; k++)
+                for (int n = 0; n < n_neu; ++n) {
+                    u[n] += (input[i][k] - W[k][n]) * (input[i][k] - W[k][n]);
+                }
+            for (int n = 0; n < n_neu; ++n) {
+                u[n] /= 2 * raio * raio;
+                u[n] = exp(-u[n]);
+            }
+
+            err = 0;
+            for (int k = 0; k < n_out; ++k) {
+                for (int n = 0; n < n_neu; ++n){
+                    o[k] += m[n][k] * u[n];
+                }
+                err += (o[k] - output[i][k]) * (o[k] - output[i][k]);
+                fprintf(fnet, "%e ", o[k]);
+                fprintf(fdes, "%e ", output[i][k]);
+            }
+            fprintf(fnet, "\n");
+            fprintf(fdes, "\n");
+
+            errT += err / n_out;
+        }
+
+        printf("err = %e\n", 0.5 * errT);
+
+        fclose(fdes);
+        fclose(fnet);
     }
 
     return 0;
