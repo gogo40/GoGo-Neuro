16ef8ee515eaa2aaabbd00152cec52d1e60ee10c
unknown <lprad@Lprad-Rafael.(none)>
2012-05-25 00:29:59 -0300

    Adicionada rede de base radial

---
+++ rbf.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    if (argc != 5) {
+        fprintf(stderr,"Usage: %s <treino?> <arquivo de treino ou arquivo de teste?> <numero de entradas> <numero de neuronios>\n");
+        return 0;
+    }
+
+    int n_in, n_out;
+    int n_neu;
+    char* filename;
+    int is_train;
+
+    sscanf
+    return 0;
+}
+
