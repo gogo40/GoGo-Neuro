b988f22a4859ff83236f4512ef499d487331cef8
Péricles Lopes Machado <pericles.raskolnikoff@gmail.com>
2012-06-02 15:45:04 -0300

    adas

--- t8/testa_rbf_8.sh
+++ t8/testa_rbf_8.sh
@@ -1,11 +1,14 @@
-../rbf 0 ../t7_final/t7.dat.v 2 1 10 0.1 t8.rbf.rna
+echo "Numero de neuronios:"$1
+echo "Beta:"$2
+
+../rbf 0 ../t7_final/t7.dat.v 2 1 $1 $2 t8.rbf.rna
 mv f_0.rna.out f_0.rna.out.v
 mv f_0.rna.sol f_0.rna.sol.v
 
-../rbf 0 ../t7_final/t7.dat.tr 2 1 10 0.1 t8.rbf.rna
+../rbf 0 ../t7_final/t7.dat.tr 2 1 $1 $2 t8.rbf.rna
 mv f_0.rna.out f_0.rna.out.tr
 mv f_0.rna.sol f_0.rna.sol.tr
 
-../rbf 0 ../t7_final/t7.dat.t 2 1 10 0.1 t8.rbf.rna
+../rbf 0 ../t7_final/t7.dat.t 2 1 $1 $2 t8.rbf.rna
 mv f_0.rna.out f_0.rna.out.t
 mv f_0.rna.sol f_0.rna.sol.t
--- t8/treina_rbf_8.sh
+++ t8/treina_rbf_8.sh
@@ -1,1 +1,6 @@
-../rbf 1 ../t7_final/t7.dat.tr.f 2 1 10 0.1 t8.rbf.rna
+echo "Numero de neuronios:"$1
+echo "Beta:"$2
+
+../rbf 1 ../t7_final/t7.dat.tr.f 2 1 $1 $2 t8.rbf.rna
+
+sh testa_rbf_8.sh $1 $2
