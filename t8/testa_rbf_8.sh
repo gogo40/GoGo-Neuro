6f8e2b823ab984192cf54750189a328101842c61
Péricles Lopes Machado <pericles.raskolnikoff@gmail.com>
2012-06-02 14:57:06 -0300

    aprimodaro

---
+++ t8/testa_rbf_8.sh
@@ -0,0 +1,11 @@
+../rbf 0 ../t7_final/t7.dat.v 2 1 10 0.1 t8.rbf.rna
+mv f_0.rna.out f_0.rna.out.v
+mv f_0.rna.sol f_0.rna.sol.v
+
+../rbf 0 ../t7_final/t7.dat.tr 2 1 10 0.1 t8.rbf.rna
+mv f_0.rna.out f_0.rna.out.tr
+mv f_0.rna.sol f_0.rna.sol.tr
+
+../rbf 0 ../t7_final/t7.dat.t 2 1 10 0.1 t8.rbf.rna
+mv f_0.rna.out f_0.rna.out.t
+mv f_0.rna.sol f_0.rna.sol.t
---
+++ t8/treina_rbf_8.sh
@@ -0,0 +1,1 @@
+../rbf 1 ../t7_final/t7.dat.tr.f 2 1 10 0.1 t8.rbf.rna
